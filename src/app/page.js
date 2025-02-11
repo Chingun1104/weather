0f95299efedc4d7fe09348b6f381279c06f0d1c2
24LP6392 24LP6392 <24LP6392@G303-CC0155.local>
2025-02-11 12:43:34 +0800

    sdfds

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,50 @@
 export default function Home() {
   return (
-    <div class='w-screen flex' >
-      <div class="bg-gray-100 w-1/2 h-screen">
-        <input class=" m-[20px] rounded-full p-4   " placeholder="Search" >
-        </input>
-        <div class='   w-[414px] h-[828px]  bg-black left-[34px] p-12  '>
-           
-          </div>
-      </div>
-      <div class='bg-slate-950 w-1/2 '>
-
-
-
+    <div class=''>
+      <Searchbar/>
+      <div class='w-screen h-screen flex  '>
+        <Whiteside/>
+        <Blackside/>
+        
 
       </div>
-
     </div>
   );
+}
+{/* <div class='w-screen flex' >
+<div class="bg-gray-100 w-1/2 h-screen">
+  <input class=" m-[20px] rounded-full p-4   " placeholder="Search" >
+  </input>
+  <div class='   w-[414px] h-[828px]  bg-black left-[34px] p-12  '>
+               
+    </div>
+</div>
+<div class='bg-slate-950 w-1/2 '>
+
+
+
+
+</div>
+
+</div> */}
+
+function Searchbar() {
+   return (
+    <input placeholder="Search" class=" m-[20px] rounded-full h-[45px] w-[250px] bg-pink-600">
+    </input>
+    )
+}
+function Blackside () {
+  return(
+    <div class='bg-gray-600 w-1/2 h-full'>
+
+    </div>
+  )
+}
+function Whiteside () {
+  return(
+    <div class='bg-white w-1/2 h-full'>
+
+    </div>
+  )
 }
